501cde181f3cf36110e55ed9f2668e24983ffac2
theplantinthedesk <5021162@it.fcrit.ac.in>
2025-11-26 23:44:40 +0530

    Fix import error

--- app/api/tours/[id]/route.ts
+++ app/api/tours/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import { Tour } from '../tours/route';
+import { Tour } from '../route';
 
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
